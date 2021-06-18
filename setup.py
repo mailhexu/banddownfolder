35284d2d88c3b83436b16bc6bb910197fe126be1
hexu <mailhexu@gmail.com>
2021-06-18 10:56:47 +0200

    requrie sisl>01.0

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='banddownfolder',
-    version='0.1',
+    version='0.1.1',
     description='Downfold Hamiltonian',
     author='Xu He',
     author_email='mailhexu@gmail.com',
     license='GPLv3',
     packages=find_packages(),
     package_data={},
-    install_requires=['numpy>1.16', 'scipy',  'matplotlib', 'ase>=3.19'
-        'netcdf4', 'sisl', 'phonopy'
+    install_requires=['numpy>=1.20', 'scipy',  'matplotlib', 'ase>=3.19'
+        'netcdf4', 'sisl>=0.10', 'phonopy'
         ],
     scripts=['scripts/phonopy_to_netcdf.py'
         ],
